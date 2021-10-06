fec4cf2d1eb3dd3d8d8edc0463e46b6cdfe20dfc
Arcadii Rubailo <rubailo.arcadii@gmail.com>
2021-10-06 16:03:58 +0200

    Use Swift 5.5

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
@@ -16,7 +16,7 @@ let package = Package(
         .binaryTarget(
             name: "common",
             url: "https://github.com/elderanakain/daily-dish-package/common-1.0.9.zip",
-            checksum: "9dadfdb5c26c80e4ee57b6d6df3ecb559776615bc61b0586dfe17123dd8fdfe6"
+            checksum: "e516c0e12e58a067e00bdfed46fc0021bafc5456ea212c67c1c3ec6cade3eb89"
         ),
     ]
 )
