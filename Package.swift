8229289582bc7a5f92cc94e2b46213c321e7efc7
Arcadii Rubailo <rubailo.arcadii@gmail.com>
2021-10-06 16:13:29 +0200

    Switch back to local distribution

--- Package.swift
+++ Package.swift
@@ -15,8 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "common",
-            url: "https://github.com/elderanakain/daily-dish-package/common-1.0.9.zip",
-            checksum: "4b85128081a33a93ad311b5c3d7f9d3f22bbf3dd067b054afb942d64b29ecf53"
+            path: "./common.xcframework"
         ),
     ]
 )
